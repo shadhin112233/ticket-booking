56f2d65283c49808d796eb681ebfd30673bf57ee
shadhin <sharoareshadhin271@gmail.com>
2025-01-16 18:15:01 +0600

    javascript

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ for (let i = 0; i < 59; i++) {
          <label for="s${i}" class="seat ${booked}"></label>`
     );
 }
+let tickets = seats.querySelectorAll("input")
+tickets.forEach((ticket)=>{
+    ticket.addEventListener("change",()=>{
+        let amount = document.querySelector(".amount").innerHTML;
+        let count = document.querySelector(".count").innerHTML;
+
+        amount = Number(amount);
+        count= Number(count);
+
+        if(ticket.checked){
+            count +=1;
+            amount +=200;
+        }
+        else{
+            count -=1;
+            amount -=200;
+        }
+        document.querySelector(".amount").innerHTML = amount;
+        document.querySelector(".count").innerHTML = count;
+    })
+})
